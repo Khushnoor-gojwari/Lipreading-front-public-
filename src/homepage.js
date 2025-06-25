406ddd208dc83b0e4488401e122777016d802d0d
Khushnoor Farooq <kkhatim43@gmail.com>
2025-06-25 18:42:30 +0530

    Update homepage.js

--- src/homepage.js
+++ src/homepage.js
@@ -73,7 +73,7 @@ useEffect(() => {
   if (!selectedVideo) return;
   setLoading(true);
   try {
-    const res = await axios.get("https://lipreadingbackend-public-9.onrender.com/predict", {
+    const res = await axios.get("https://lipreadingbackend-public-9.onrender.com/predict/", {
       params: { video_name: selectedVideo },
     });
 
